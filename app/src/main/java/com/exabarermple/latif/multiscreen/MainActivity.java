137b7a64e787f3aa03cef99c1605bc4a7e7070b9
latif <latif.hamid@cutesafe.com>
2019-05-31 11:29:07 +0300

    test

---
+++ app/src/main/java/com/exabarermple/latif/multiscreen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.exabarermple.latif.multiscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
+       // test
+    }
+}
