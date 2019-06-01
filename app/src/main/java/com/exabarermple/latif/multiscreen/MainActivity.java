df902cc9c4008a705ebe113282e432285a37e0db
latif <latif.hamid@cutesafe.com>
2019-06-01 15:30:26 +0300

    xml onClick future

--- app/src/main/java/com/exabarermple/latif/multiscreen/MainActivity.java
+++ app/src/main/java/com/exabarermple/latif/multiscreen/MainActivity.java
@@ -1,7 +1,12 @@
 package com.exabarermple.latif.multiscreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
-       // test
+
     }
+    /** to open a new activity we can use the onClick future of xml file
+     we add   android:onClick="xxxxx"  on the xml part  then we call it by creating xxxxx method
+     below we add android:onClick="number"  on the xml part  then we call it by creating number method  */
+    public void number(View view){
+        Intent numberIntent = new Intent ( MainActivity.this,NumbersActivity.class );
+        startActivity ( numberIntent );
+
+
+    }
+
 }
