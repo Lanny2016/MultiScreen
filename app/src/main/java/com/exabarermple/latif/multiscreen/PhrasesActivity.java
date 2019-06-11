2cea88d146c38910a6dbd023f5b5e39d6c09e31c
latif <latif.hamid@cutesafe.com>
2019-06-11 15:03:50 +0300

    public static int/String... to decide if the value should be displayed in the list

--- app/src/main/java/com/exabarermple/latif/multiscreen/PhrasesActivity.java
+++ app/src/main/java/com/exabarermple/latif/multiscreen/PhrasesActivity.java
@@ -2,12 +2,63 @@ package com.exabarermple.latif.multiscreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_phrases );
+        setContentView ( R.layout.worlist );
+
+
+        /** make an Arraylist in java
+         * we can make different types ArrayLists according to our needs so data types can be different
+         * such as  ArrayList<String> xxxxxx = new ArrayList<String>();
+         * ArrayList<Integer> xxxxxx = new ArrayList<Integer>()*/
+
+        /** below we use Word class which contains 2 objects such as mEnglish and mTurkish instead of String in the ArrayList
+         * ArrayList<String> words = new ArrayList<String>();
+         * ArrayList<Word> words = new ArrayList<Word>(); */
+
+        ArrayList<Word> words = new ArrayList<> ();
+
+        // adding value to the ArrayList
+        //words.add ( "one");
+
+        /** now we create a new word object and add word objects in the list*/
+
+        Word w = new Word ( "Hi","Merhaba");
+        // add it in the words list
+        words.add ( w );
+        // or
+        words.add ( new Word ( "How are you?","Nasılsın?"));
+        words.add ( new Word ( "How old are you?","Kaç yaşındasın?"));
+        words.add ( new Word ( "Where are you from?","Nerelisin?"));
+        words.add ( new Word ( "What is your name?","Adın ne?" ));
+        words.add ( new Word ( "Are you married?","Evli misin?"));
+        words.add ( new Word ( "How many people are there in your family?","Ailende kaç kişivar?"));
+        words.add ( new Word ( "Do you have any children?","Çocuğun var mı?"));
+        words.add ( new Word ( "What are their names?","Onların adları ne?"));
+
+        /** displaying values on a listView by using a ArrayAdapter
+         * below we made an ArrayAdapter which uses <String> in it*/
+
+        // ArrayAdapter<String> itemsAdapter = new ArrayAdapter<> ( this,android.R.layout.simple_list_item_1,words );
+        /** To display more value on a listView we can make a custom layout and make a custom class that can contain more
+         * objects in then we we will use in ArrayAdapter because ArrayAdapter can only have one dataType at a time*/
+
+        //ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String> ( this,android.R.layout.simple_expandable_list_item_1,words );
+        // we update the ArrayAdapter
+        //  ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<Word> ( this,R.layout.custom_list_view,words );
+        // now we use custom wordAdapter we made
+        wordAdapter adapter = new wordAdapter (this,words);
+        // casting listView
+
+        ListView list_view = (ListView)findViewById ( R.id.listView );
+        list_view.setAdapter ( adapter );
+
     }
 }
--- app/src/main/java/com/exabarermple/latif/multiscreen/Word.java
+++ app/src/main/java/com/exabarermple/latif/multiscreen/Word.java
@@ -13,15 +13,40 @@ public class Word {
     private String mTurkish;
     /** image for numbers*/
     private int imageForNumbers;
+    /** we use  public static final to make constant in java whose value can not be changed any more
+     * once it is assigned. because it is constant we declare it with a capital letter
+     * below we make int to -1 because that is the only exception in here for other possibility*/
+    public static final int NO_IMAGE_PROVIDED = -1;
+    /** recourseId for different backgrounds*/
+    private int NumbersActivityBackgroundColor;
+    private int ColorActivityBackgroundColor;
+    private int FamilyActivityBackgroundColor;
+    private int PhraseActivityBackgroundColor;
 
     public Word(String mEnglish, String mTurkish, int imageForNumbers) {
         this.mEnglish = mEnglish;
         this.mTurkish = mTurkish;
         this.imageForNumbers = imageForNumbers;
     }
+    /** we need to add two more constructors  because three of the activities need to have images
+     * in the listView and different color background
+     * and the phrase activity no image in listView*/
+
+    public Word(int NumbersActivityBackgroundColor,int ColorActivityBackgroundColor,int FamilyActivityBackgroundColor,
+                int PhraseActivityBackgroundColor){
+        this.ColorActivityBackgroundColor = ColorActivityBackgroundColor;
+        this.NumbersActivityBackgroundColor = NumbersActivityBackgroundColor;
+        this.FamilyActivityBackgroundColor = FamilyActivityBackgroundColor;
+        this.PhraseActivityBackgroundColor = PhraseActivityBackgroundColor;
+    }
+
+    public Word(String mEnglish,String mTurkish){
+        this.mEnglish = mEnglish;
+        this.mTurkish = mTurkish;
+
+    }
 
     /** get method for mEnglish translation*/
-
 
     public String getmEnglish() {
         return mEnglish;
@@ -34,4 +59,9 @@ public class Word {
     public int getImageForNumbers() {
         return imageForNumbers;
     }
+
+    public int getNumbersActivityBackgroundColor(){return NumbersActivityBackgroundColor;}
+    public int getColorActivityBackgroundColor(){return ColorActivityBackgroundColor;}
+    public int getFamilyActivityBackgroundColor(){return FamilyActivityBackgroundColor;}
+    public int getPhraseActivityBackgroundColor(){return PhraseActivityBackgroundColor;}
 }
